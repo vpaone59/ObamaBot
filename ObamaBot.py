e3f605b0b18fde58430b19f02c92d1d6fc5bcdb1
Vincent Paone <55220731+vpaone59@users.noreply.github.com>
2022-07-26 19:52:23 -0400

    added fibonacci function and cleaned up main
    added fibonacci function and cleaned up main

--- ObamaBot.py
+++ ObamaBot.py
@@ -1,48 +1,68 @@
 # Vincent Paone
 # Project began 4/20/2022 --
 # ObamaBot - main file
-#  
+#
 # There is no serious political offiliation. This is all in good fun.
 
 from array import array
 from ast import alias
-import time, os, re, json, discord, asyncio
+import time
+import os
+import re
+import json
+import discord
+import asyncio
 from tokenize import String
 from discord.ext import commands
 from discord.utils import get
 
-# search for the config.json file in the current directory and load the file
-if os.path.exists(os.getcwd() + "/config.json"):
-    with open("./config.json") as f:
+
+# ************ INITIAL SETUP --- YOU SHOULD NOT TOUCH THIS SECTION ************
+print(f"*********** STARTING OBAMABOT IN " + os.getcwd() + " ***********")
+
+if os.path.exists(os.getcwd() + "/ObamaBot/config.json"):
+    print("***** config path exists *****")
+    with open(os.getcwd() + "/ObamaBot/config.json") as f:
+        print("***** opened config *****")
         configData = json.load(f)
 
 # if the config file doesn't exist within the current directory, use the template to create the file automatically
 else:
+    print("***** config path DOES NOT exist *****")
     configTemplate = {"Token": "", "Prefix": "", "bannedWords": []}
-
-    with open(os.getcwd() + "/config.json", "w+") as f:
-        json.dump(configTemplate, f) # dump the configTemplate to the new config.json
+    with open(os.getcwd() + "/ObamaBot/config.json", "w+") as f:
+        print("***** new config being created using template *****")
+        # dump the configTemplate to the new config.json
+        json.dump(configTemplate, f)
+        print("***** config created *****")
 
 # grab some info from the config file
 TOKEN = configData["TOKEN"]
 bannedWords = configData["bannedWords"]
 prefix = configData["Prefix"]
+# search for the config.json file in the current directory and load the file
+
 
 # search for the acc.json file in the current directory and load the file
-if os.path.exists(os.getcwd() + "/acc.json"):
-    with open("./acc.json") as f:
+if os.path.exists(os.getcwd() + "/ObamaBot/acc.json"):
+    print("***** acc path exists *****")
+    with open(os.getcwd() + "/ObamaBot/acc.json") as f:
+        print("***** opened acc *****")
         accData = json.load(f)
 
-    # if the acc file doesn't exist use the template to create the file
+# if the acc file doesn't exist use the template to create the file
 else:
+    print("***** acc path DOES NOT exist *****")
     accTemplate = {"Usernames": [], "Passwords": [], "Account_name": []}
+    with open(os.getcwd() + "/ObamaBot/acc.json", "w+") as f:
+        print("***** new acc being created using template *****")
+        # dump the accTemplate to the new acc.json
+        json.dump(accTemplate, f)
+        print("***** acc created *****")
 
-    with open(os.getcwd() + "/acc.json", "w+") as f:
-        json.dump(accTemplate, f) # dump the accTemplate to the new acc.json  
-        
 # grab some info from the acc file
 accUsers = accData["Usernames"]
-accPasses = accData["Passwords"]    
+accPasses = accData["Passwords"]
 accNick = accData["Account_name"]
 
 
@@ -53,20 +73,26 @@ client = commands.Bot(command_prefix=prefix)
 # we need this to check the bot connected properly in the on_ready fxn
 full_ready = False
 
+# ************ INITIAL SETUP --- YOU SHOULD NOT TOUCH THIS SECTION ************
+
+
 # wait for the bot to FULLY connect to the server
 # once bot connects print message in local terminal
 @client.event
 async def on_ready():
-    print('$$$$$ We have logged in as {0.user} $$$$$'.format(client))
-    print(f'$$$$$ {client} $$$$$')
+    print(
+        'from on_ready: $$$$$ We have logged in as {0.user} $$$$$'.format(client))
+    print(f'from on_ready: $$$$$ {client} $$$$$')
     full_ready = True
 
     if full_ready == True:
-        print('$$$$$ Loading Cogs...standby $$$$$')
+        print('from on_ready: $$$$$ Loading Cogs...standby $$$$$')
         loadCogs()
+        print('from on_ready: $$$$$ Cogs loaded successfully $$$$$')
     else:
-        print('$$$$$ Bot not ready, Cogs not loaded $$$$$')
-            
+        print('from on_ready: $$$$$ Bot not ready, Cogs not loaded $$$$$')
+
+
 # every time there is a message in any channel in any guild, this runs
 # param: message - The message content that was last sent
 @client.event
@@ -85,24 +111,23 @@ async def on_message(message):
     # server name the message was sent from
     guild = str(message.guild.name)
 
-    
-    # on every message print it to the terminal
+    # on every message **** IN LOGS CHANNEL **** print it to the terminal
     # print the username, the message, the channel it was sent in and the server (guild)
     # print(f'{username}: {user_message} [userid= {user_id} (channel= {channel})]')
-    print(f'{username}: {user_message} \t(channel= {channel} on server= {guild})')
-    
+    if channel == 'logs':
+        print(f'{username}: {user_message} \t(channel= {channel} on server= {guild})')
+
     # ignore messages sent from the bot itself
     # prevents infinite replying
     if message.author == client.user:
         return
 
-
     # ensures the message sent did not contain a banned word
     # allow the message if it starts with command tag -rmvbannedword
     if not user_message.lower().startswith("-rmvbannedword"):
-        if bannedWords != None: # ensure the list isn't empty
+        if bannedWords != None:  # ensure the list isn't empty
             for word in bannedWords:
-                if msg_contain_word(message.content.lower(), word): 
+                if msg_contain_word(message.content.lower(), word):
                     await message.delete()
                     await message.channel.send(f"```{messageAuthor.mention} You used a banned word therefore your message was removed.```")
                     await message.channel.send(f"```{messageAuthor.mention} Obama is telling your Mama! Please do not use banned words!```")
@@ -146,45 +171,47 @@ async def on_message(message):
     elif user_message.lower().startswith('who asked'):
         await message.channel.send(file=discord.File('gifs/obama-shrug.gif'))
         return
-    
+
     # if the message starts with 'horny'
     elif user_message.lower().startswith('horny'):
         await message.channel.send(file=discord.File('gifs/obama-lip_bite.jpg'))
         return
-    
+
     # necessary to process anything the bot will do
     await client.process_commands(message)
+
 
 # Function to load all Cogs that live in the cogs folder
 # Ran on Bot startup
 def loadCogs():
-    for filename in os.listdir('./cogs'):
+    for filename in os.listdir(os.getcwd() + '/ObamaBot/cogs'):
         if filename.endswith('.py'):
             try:
                 # -3 cuts the .py extension from filename
-                client.load_extension(f'cogs.{filename[:-3]}') 
+                client.load_extension(f'cogs.{filename[:-3]}')
                 print(f'----- Cog {filename} loaded -----')
             except commands.ExtensionAlreadyLoaded:
                 print(f'----- Cog {filename} aleady loaded -----')
 
+
 # Function to unload all Cogs in the cogs folder
 # Runs on -rl all
 def unloadCogs():
-    for filename in os.listdir('./cogs'):
+    for filename in os.listdir(os.getcwd() + '/ObamaBot/cogs'):
         if filename.endswith('.py'):
             try:
                 client.unload_extension(f'cogs.{filename[:-3]}')
                 print(f'----- Cog {filename} unloaded successfully -----')
             except commands.ExtensionNotLoaded:
                 print(f'----- Cog {filename} is not loaded -----')
-            
+
 
 # return true if there is a banned word in the message
 # but will not remove attached characters i.e. will remove 'Tom' not 'Tommas'
 # \b matches the empty string but only at the beginning or end of the word
-# https://docs.python.org/3/library/re.html        
+# https://docs.python.org/3/library/re.html
 def msg_contain_word(msg, word):
-        return re.search(fr'\b({word})\b', msg) is not None
+    return re.search(fr'\b({word})\b', msg) is not None
 
 
 # Load a Cog file
@@ -227,15 +254,15 @@ async def unload(ctx, extension):
 # param: ctx - The context in which the command has been executed
 # param: extension - The name of the Cog file to reload
 # do -rl all to unload/load all Cogs
-@client.command(aliases= ['rl'], description= 'Reloads all Cog files')
+@client.command(aliases=['rl'], description='Reloads all Cog files')
 @commands.has_permissions(administrator=True)
 async def refreshload(ctx, extension):
-    
+
     if extension == 'all':
         unloadCogs()
         await ctx.send(f'``` All Cogs unloaded successfully```')
         time.sleep(2)
-        loadCogs()    
+        loadCogs()
         await ctx.send(f'``` All Cogs loaded successfully```')
     else:
         try:
@@ -249,7 +276,7 @@ async def refreshload(ctx, extension):
             await ctx.send(f'```Cog {extension}.py loaded```')
         except commands.ExtensionNotFound:
             await ctx.send(f'```Cog {extension}.py not in directory```')
-        
+
 
 # add a banned word to the bannedWords list in the json config
 # only admin should be able to run this
@@ -260,18 +287,18 @@ async def refreshload(ctx, extension):
 # message can only be sent 1 time, every 3 seconds, per user.
 @commands.cooldown(1, 3, commands.BucketType.user)
 async def banword(ctx, word):
-    #check if the word is already banned
+    # check if the word is already banned
     if word.lower() in bannedWords:
         await ctx.send("```Already banned```")
     else:
         bannedWords.append(word.lower())
-        #add it to the list
+        # add it to the list
         with open("./config.json", "r+") as f:
             data = json.load(f)
             data["bannedWords"] = bannedWords
-            f.seek(0) 
-            f.write(json.dumps(data)) 
-            f.truncate() #resizes file
+            f.seek(0)
+            f.write(json.dumps(data))
+            f.truncate()  # resizes file
 
         await ctx.send("```Word added to banned words list```")
 
@@ -280,7 +307,7 @@ async def banword(ctx, word):
 # only admin should be able to run this
 # param: ctx The context of which the command is entered
 # param: word - The word you want to remove from the banned words list
-@client.command(aliases = ['unban'])
+@client.command(aliases=['unban'])
 @commands.has_permissions(administrator=True)
 # message can only be sent 1 time, every 3 seconds, per user.
 @commands.cooldown(1, 3, commands.BucketType.user)
@@ -297,9 +324,10 @@ async def rmvbannedword(ctx, word):
 
         await ctx.send("```Word removed from banned words list```")
 
-    #if the word isn't in the list    
+    # if the word isn't in the list
     else:
         await ctx.send("```Word isn't banned```")
+
 
 # save information, username/password/nickname into a json
 # only admin should be able to run this
@@ -308,7 +336,7 @@ async def rmvbannedword(ctx, word):
 @commands.has_permissions(administrator=True)
 @commands.cooldown(1, 3, commands.BucketType.user)
 async def saveAccount(ctx):
-    
+
     # prompt #1 for username entry
     await ctx.send(f"Enter in the Username you wish to add {ctx.author.mention}")
     try:
@@ -316,8 +344,7 @@ async def saveAccount(ctx):
         username = str(user_message.content)
     except asyncio.TimeoutError:
         await ctx.channel.send("```ERROR: Timeout Exception```")
-    
-        
+
     # prompt #2 for password entry
     await ctx.send(f"Enter in the Password you wish to add {ctx.author.mention}")
     try:
@@ -325,71 +352,68 @@ async def saveAccount(ctx):
         password = str(user_message.content)
     except asyncio.TimeoutError:
         await ctx.channel.send("```ERROR: Timeout Exception```")
-        
-            
+
     # check if the account info is already in the list
     if username.lower() in accUsers and password in accPasses:
         userindex = accUsers.index(username)
         passindex = accPasses.index(password)
-        
+
         if userindex == passindex:
             await ctx.send(f"```Account info already exists at position indices {userindex} and {passindex}\n It will not be appended.```")
     else:
-        #add it to the list
+        # add it to the list
         accUsers.append(username.lower())
         accPasses.append(password)
         with open("./acc.json", "r+") as f:
             data = json.load(f)
             data["Usernames"] = accUsers
             data["Passwords"] = accPasses
-            f.seek(0) 
-            f.write(json.dumps(data)) 
-            f.truncate() #resizes file
-               
+            f.seek(0)
+            f.write(json.dumps(data))
+            f.truncate()  # resizes file
+
     # prompt #3 for nickname entry
     await ctx.send(f"```Provide a nickname for this entry? Reply Y/N```")
     user_message = await client.wait_for('message', timeout=15, check=lambda message: message.author == ctx.author)
     user_reply = str(user_message.content)
-    
+
     if user_reply.lower() == "y":
         await ctx.send("What would you like to name this entry?")
         user_message = await client.wait_for('message', timeout=15, check=lambda message: message.author == ctx.author)
         nickname = str(user_message.content)
-        
+
         if nickname.lower() in accNick:
             await ctx.send("Nickname already exists in the list, default will be applied")
-            #have them try again, and enter Default for a default nickname to apply
+            # have them try again, and enter Default for a default nickname to apply
     else:
-        #if user does not want to apply a nickname a default will be chosen
+        # if user does not want to apply a nickname a default will be chosen
         await ctx.send("```No nickname chosen. Default will be applied.```")
         nickname = username + '#' + str(accUsers.index(username))
-    
+
     accNick.append(nickname)
     with open("./acc.json", "r+") as f:
         data = json.load(f)
         data["Account_name"] = accNick
-        f.seek(0) 
-        f.write(json.dumps(data)) 
-        f.truncate() #resizes file
-    
-    
+        f.seek(0)
+        f.write(json.dumps(data))
+        f.truncate()  # resizes file
+
     await ctx.send(f"```Added the following account information:\n Username: {username}\n Password: {password}\n Nickname: {nickname}```")
-    
-    
+
 
 # clear all data from a json file the user chooses
 # only admin should be able to run this
 # param: ctx The context of which the command is entered
 # param: data_file The json file which the user wants wiped of data
-@client.command(aliases = ['clean'])
+@client.command(aliases=['clean'])
 @commands.has_permissions(administrator=True)
 # message can only be sent 1 time, every 5 seconds, per user.
 @commands.cooldown(1, 5, commands.BucketType.user)
 async def cleardata(ctx, data_file):
-    
+
     data_file = "/" + data_file + ".json"
     print(data_file)
-    
+
     if os.path.exists(os.getcwd() + data_file):
         print("true")
         with open("." + data_file, "r") as f:
@@ -398,12 +422,13 @@ async def cleardata(ctx, data_file):
                 data.pop(o)
                 await ctx.send(f"Removed {o}")
                 print("true")
-            
+
             f.seek(0)
             f.write(json.dumps(data))
             f.truncate()
-            
+
     else:
         print("false")
-             
+
+
 client.run(TOKEN)
--- cogs/general.py
+++ cogs/general.py
@@ -1,17 +1,22 @@
-import discord, time, json, os
+import discord
+import time
+import json
+import os
 from discord.ext import commands
 from discord.utils import get
+import asyncio
 
-# general commands for ObamaBot
-# Cogs for Obama Bot
-# Cogs are a way to hide standard commands, events, functions etc 
-# in another .py file
+# general commands for any Bot
+# Cogs are a way to hide standard commands, events, functions etc in another .py file
 
 
 class general(commands.Cog):
 
     def __init__(self, client):
-        self.client = client    
+        self.client = client
+
+    # hello command - Bot replies hello and @mentions the user
+    # use -hello or -hi
 
     @commands.command(aliases=['hi'])
     @commands.cooldown(1, 3, commands.BucketType.user)
@@ -19,67 +24,81 @@ class general(commands.Cog):
         user_id = ctx.author
         await ctx.send(f'Hello {user_id.mention}')
 
-
+    # hello all command - Bot @mentions @all users and says hello
+    # use -hello0 or -hiall
     @commands.command(aliases=['hiall'])
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def helloO(self, ctx):
-        user_id = ctx.author
         await ctx.send(f'Hello {ctx.message.guild.default_role}')
-    
 
-    @commands.command()
+    # ping command - Bot replies with the current latency
+    # use -ping
+    @commands.command(aliases=['p'])
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def ping(self, ctx):
         client_latency = round(self.client.latency * 1000, 2)
-        await ctx.send(f'ObamaPong! {client_latency}ms')
+        await ctx.send(f'pong {client_latency}ms')
 
-        if client_latency > 100:
-            time.sleep(1)
-            await ctx.send('YIKES!')
-        elif client_latency < 100 and client_latency >= 60:
-            time.sleep(1)
-            await ctx.send('Sloooow')
-        elif client_latency < 60 and client_latency >= 30:
-            time.sleep(1)
-            await ctx.send('Average')
-        else:
-            time.sleep(1)
-            await ctx.send('Get a load of this guy!')
-
-    # check if the user has the American role
-    # param: @user. ex, -ia @user
-    @commands.command(aliases=['ia'])
+    # check if the user has the American role. This can be changed to anything
+    # param: @user. ex, -rolecheck @user
+    @commands.command(aliases=['rolecheck'])
     @commands.cooldown(1, 3, commands.BucketType.user)
-    async def isAmerican(self, ctx, user: discord.Member):
-        role = discord.utils.find(lambda r: r.name == 'American', ctx.message.guild.roles)
+    async def hasRole(self, ctx, user: discord.Member):
+        # change 'American' to any role
+        role = discord.utils.find(
+            lambda r: r.name == 'American', ctx.message.guild.roles)
         if role in user.roles:
-            await ctx.send(f"User {user.mention} is a PATRIOT!!")   
+            await ctx.send(f"User {user.mention} has the role")
         else:
-            await ctx.send(f"User {user.mention} is NOT A TRUE CHRISTIAN AMERICAN!!")
-        
+            await ctx.send(f"User{user.mention} does not have the role")
 
-    # @commands.command(aliases=['makerole'])
-    # @commands.cooldown(1, 3, commands.BucketType.user)
-    # async def createrole(self, ctx, role):
-    #     if get(ctx.guild.roles, name=role):
-    #         await ctx.send("Role already exists!")
-    #     else:
-    #         await ctx.guild.create_role(name="American", color=discord.Color(0x0062ff))    
+    # calculate Fibonacci sequence
+    # param: num - the number sequence to calculate
 
-    
-    # @commands.command()
-    # async def list(self, ctx):
-    #     # for command in self.client.command:
-    #     #     await ctx.send(f'{command}')
-    #     command_list = []
-    #     all_commands = [command.name for command in self.client.commands]
-    #     for c in all_commands:
-    #         command_name = c
-    #         command_list = command_list + "\n" + command_name
-    #     if not command_list:
-    #         await ctx.send(f'no commands available')
-    #     else:
-    #         await ctx.send(f'{command_list}')
+    @commands.command()
+    async def fib(self, ctx, num):
+        # message input comes in as a string, so we cast to int
+        n = int(num)
+        result = fibonacci(n)
+        await ctx.send(f"```Result:\nFibonnaci of {num} = {result}```")
+
 
 def setup(client):
     client.add_cog(general(client))
+
+
+# fibonacci function for the fib command
+def fibonacci(n):
+    if n < 0:
+        print(f"User input {n}: negative not allowed ")
+    elif n == 0:
+        print(f"User input {n}: Fib({n}) = 0")
+        return 0
+    elif n == 1 or n == 2:
+        print(f"User input {n}: Fib({n}) = 1")
+        return 1
+    else:
+        fib = fibonacci(n-1) + fibonacci(n-2)
+        return fib
+
+# @commands.command(aliases=['makerole'])
+# @commands.cooldown(1, 3, commands.BucketType.user)
+# async def createrole(self, ctx, role):
+#     if get(ctx.guild.roles, name=role):
+#         await ctx.send("Role already exists!")
+#     else:
+#         await ctx.guild.create_role(name="American", color=discord.Color(0x0062ff))
+
+# @commands.command()
+# async def list(self, ctx):
+#     # for command in self.client.command:
+#     #     await ctx.send(f'{command}')
+#     command_list = []
+#     all_commands = [command.name for command in self.client.commands]
+#     for c in all_commands:
+#         command_name = c
+#         command_list = command_list + "\n" + command_name
+#     if not command_list:
+#         await ctx.send(f'no commands available')
+#     else:
+#         await ctx.send(f'{command_list}')

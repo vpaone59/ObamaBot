90a6487a448722cb2f52e6a295f09826203c5447
DESKTOP-U9L2BOL\vpaon <vpaone59@gmail.com>
2023-04-12 21:57:57 -0400

    Full working custom task command

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -45,6 +45,9 @@ class Tasks(commands.Cog):
 
     @tasks.loop(seconds=0, minutes=1, hours=0)
     async def custom_task(self, channel, content):
+        """
+        Custom task created by the user. Only one of these can exist at a time
+        """
         await channel.send(f'{content}')
 
     @commands.command(aliases=['cct'])
@@ -52,67 +55,77 @@ class Tasks(commands.Cog):
         """
         Start a new custom task with customizable time interval
         """
-        # a check to ensure the prompts aren't interrupted by other user's / bot's messages
-        def check(message):
+        if self.custom_task.is_running():
+            await ctx.send(f'A custom task is already running! Stop this task before running another custom task. Use !tasks to see the running tasks.')
+            return
+
+        def check(message):  # a check to ensure the prompts aren't interrupted by other user's / bot's messages
             return message.author == ctx.author and message.channel == ctx.channel
 
         # prompt the user for a task name and if it already exists prompt again
         while True:
             try:
-                await ctx.send("```Name the task, must be one word. Type 'cancel' to stop task creation```")
-                task_name = await self.bot.wait_for('message', timeout=10.0, check=check)
+                await ctx.send("Name the task, must be one word. Type 'cancel' to stop task creation")
+                task_name = await self.bot.wait_for('message', timeout=30.0, check=check)
                 task_name = task_name.content.strip()
                 # if the user enters 'cancel' then the command will stop
                 if task_name == 'cancel':
-                    await ctx.send('```Canceled task creation```')
+                    await ctx.send('Canceled task creation')
                     return
                 # Check if the task name already exists
                 if task_name.lower() in [task.lower() for task in self.tasks]:
-                    await ctx.send("```A task with this name already exists. Please choose a different name. Use !tasks to see the list of currently running tasks.```")
+                    await ctx.send("A task with this name already exists. Please choose a different name. Use !tasks to see the list of currently running tasks.")
                 else:
                     break
             except asyncio.TimeoutError:
-                await ctx.send("```Task creation timed out.```")
+                await ctx.send("Task creation timed out.")
                 return
 
         # prompt the user for the content of their custom task
-        try:
-            await ctx.send("What should be sent to the channel when the task runs?")
-            task_content = await self.bot.wait_for('message', timeout=10.0, check=check)
-            task_content = task_content.content
-        except asyncio.TimeoutError:
-            await ctx.send("```Task creation timed out.```")
-            return
+        while True:
+            try:
+                await ctx.send("Enter the content of the task:")
+                task_content = await self.bot.wait_for('message', timeout=10.0, check=check)
+                task_content = task_content.content
+                if len(task_content) > 100:
+                    await ctx.send(f"Task content length too long, must be less than 100 characters. Current length {len(task_content)}")
+                else:
+                    break
+            except asyncio.TimeoutError:
+                await ctx.send("Task creation timed out.")
+                return
 
         # prompt the user for the time interval their custom task will loop on
-        await ctx.send("Enter the task interval in seconds, minutes, and hours (separated by spaces).\n\
+        while True:
+            try:
+                await ctx.send("Enter the task interval in seconds, minutes, and hours (separated by spaces).\n\
                        (seconds > 1) (minutes >= 0) (hours >= 0)")
-        try:
-            response = await self.bot.wait_for('message', timeout=60.0, check=check)
-            interval_time = response.content.strip().split()
-            if len(interval_time) == 1:
-                seconds = int(interval_time[0])
-                minutes = 0
-                hours = 0
-            elif len(interval_time) == 2:
-                seconds = int(interval_time[0])
-                minutes = int(interval_time[1])
-                hours = 0
-            elif len(interval_time) >= 3:
-                seconds = int(interval_time[0])
-                minutes = int(interval_time[1])
-                hours = int(interval_time[2])
-            if seconds <= 1 or minutes < 0 or hours < 0:
-                raise ValueError("Invalid time interval.")
-            else:
-                self.custom_task.change_interval(
-                    seconds=seconds, minutes=minutes, hours=hours)
-                self.custom_task.start(ctx.channel, task_content)
-                self.tasks.append(task_name)
-                await ctx.send(
-                    f"```A new task '{task_name}' was started in this channel and will run every {seconds} seconds, {minutes} minutes, and {hours} hours.```")
-        except Exception as e:
-            await ctx.send(f"```ERROR {e}```")
+                response = await self.bot.wait_for('message', timeout=60.0, check=check)
+                interval_time = response.content.strip().split()
+                if len(interval_time) == 1:
+                    seconds = int(interval_time[0])
+                    minutes = 0
+                    hours = 0
+                elif len(interval_time) == 2:
+                    seconds = int(interval_time[0])
+                    minutes = int(interval_time[1])
+                    hours = 0
+                elif len(interval_time) >= 3:
+                    seconds = int(interval_time[0])
+                    minutes = int(interval_time[1])
+                    hours = int(interval_time[2])
+                if seconds <= 1 or minutes < 0 or hours < 0:
+                    raise ValueError("Invalid time interval.")
+                else:
+                    self.custom_task.change_interval(
+                        seconds=seconds, minutes=minutes, hours=hours)
+                    self.custom_task.start(ctx.channel, task_content)
+                    self.tasks.append(task_name)
+                    await ctx.send(
+                        f"Task '{task_name}' was started in this channel and is running on a loop of: {hours}h: {minutes}m: {seconds}s")
+                    break
+            except Exception as e:
+                await ctx.send(f"ERROR {e}")
 
     @commands.command(aliases=['stop'])
     async def stop_task(self, ctx, task_name):
@@ -121,21 +134,18 @@ class Tasks(commands.Cog):
         """
         try:
             for task in self.tasks:
-                print(self.tasks)
-                print(task, task_name)
-                if task == task_name:
+                if task.lower() == task_name.lower():
                     # Stop the task and remove it from the task list
-                    task_to_stop = self.custom_task
-                    task_to_stop.cancel()
-                    self.tasks.remove(task_name)
-                    await ctx.send(f"```Task '{task_name}' was stopped in this channel.```")
+                    self.custom_task.cancel()
+                    self.tasks.remove(task)
+                    await ctx.send(f"```Task '{task_name}' stopped```")
                     return
-            await ctx.send(f"```Task '{task_name}' is not currently running in this channel.```")
+            await ctx.send(f"```Task '{task_name}' is not running in this channel```")
         except Exception as e:
             print(f"ERROR {e}")
             await ctx.send(f"```ERROR {e}```")
         else:
-            await ctx.send("```Task was not stopped.\nMust send the stop_task command from the same channel the task is running in. You can check the status of tasks using the tasks_status command.```")
+            await ctx.send(f"```Task '{task_name}' was *not* stopped```")
 
     @commands.command(aliases=['twm'])
     async def toggle_wednesday_meme(self, ctx):

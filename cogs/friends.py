cdf9a84b0b61cbb5979781e121be3d16edbaa90e
VINCENT <55220731+vpaone59@users.noreply.github.com>
2024-03-10 15:38:07 -0400

    Made Ethan and Walter reactions toggle-able via Discord commands

--- cogs/friends.py
+++ cogs/friends.py
@@ -10,13 +10,14 @@ import logging
 import discord
 from discord.ext import commands
 
+logger = logging.getLogger(__name__)
+
 BEE_REACTS = [
     "<:obamagiga:844300314936213565> :point_right: :bee:",
     ":bee: :broom: <:feelsobama:842634906999193620>",
     "<:obamajoy:842822700912476190> :fire: :bee: :fire:",
 ]
 FRIEND_GUILDS = os.getenv("FRIEND_GUILD_IDS")
-logger = logging.getLogger(__name__)
 
 
 class Friends(commands.Cog):
@@ -27,6 +28,10 @@ class Friends(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.ethan_enabled = False  # Flag for Ethan reactions
+        self.walter_enabled = False  # Flag for Walter reactions
+        self.dj_enabled = False  # Flag for DJ functionality
+        self.inc_enabled = False  # Flag for INC functionality
 
         # Configure the logger to save logs to bot.log
         if not logger.handlers:
@@ -50,21 +55,28 @@ class Friends(commands.Cog):
         """
         whenever a message is sent this Cog will listen and execute code below
         """
-        messageAuthor = message.author
         string_message = str(message.content)
-        # return whenever a prefix / command message is detected
+
+        # return whenever a prefix is detected without a command attached
         if string_message.startswith(f'{os.getenv("PREFIX")}'):
             return
 
         if str(message.guild.id) not in FRIEND_GUILDS:
             return
 
-        # If Ethan (ID= 166964846179385344) sends a message, react with the emoji
-        if messageAuthor.id == 166964846179385344:
+        # Ethan reactions (toggleable)
+        if self.ethan_enabled and message.author.id == 166964846179385344:
             await message.add_reaction("♿")
-            return
 
-        # if DJ (ID= 123107464240562180) is @ mentioned
+        # Walter reactions (toggleable)
+        if self.walter_enabled and message.author.id == 965414583860883456:
+            walter_int = random.randint(1, 3)
+            if walter_int == 1:
+                await message.channel.send(
+                    file=discord.File("gifs/weird/boner_alert.jpg")
+                )
+
+        # # if DJ (ID= 123107464240562180) is @ mentioned
         # for user in message.mentions:
         #     if user.id == 123107464240562180:
         #         file_name = get_random_batman_deej_file()
@@ -73,20 +85,46 @@ class Friends(commands.Cog):
         #             await message.channel.send(file=discord.File(file_path))
         #         break  # Once the user is found, we can break out of the loop
 
-        # if Walter (ID= 247936308733935616) sends a message
-        if messageAuthor.id == 965414583860883456:
-            walter_int = random.randint(1, 3)
-            if walter_int == 1:
-                await message.channel.send(
-                    file=discord.File("gifs/weird/boner_alert.jpg")
-                )
-            else:
-                return
-
         # if message.guild.id == 842545435050508328:
         #     if string_message.startswith("INC"):
         #         format_link = format_incident_link(string_message)
         #         await message.channel.send(f"{format_link}")
+
+    @commands.command()
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def toggle_ethan(self, ctx):
+        """
+        Toggle reactions for Ethan's messages.
+        """
+        self.ethan_enabled = not self.ethan_enabled
+        if self.ethan_enabled:
+            await ctx.send("Ethan reactions enabled!")
+        else:
+            await ctx.send("Ethan reactions disabled.")
+
+    @commands.command()
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def toggle_walter(self, ctx):
+        """
+        Toggle reactions for Walter's messages.
+        """
+        self.walter_enabled = not self.walter_enabled
+        if self.walter_enabled:
+            await ctx.send("Walter reactions enabled!")
+        else:
+            await ctx.send("Walter reactions disabled.")
+
+    @commands.command()
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def toggle_dj(self, ctx):
+        """
+        Toggle the DJ functionality on or off.
+        """
+        self.dj_enabled = not self.dj_enabled
+        if self.dj_enabled:
+            await ctx.send("DJ functionality enabled. Mention me to play memes!")
+        else:
+            await ctx.send("DJ functionality disabled.")
 
     @commands.command(aliases=["bee", "b"])
     @commands.cooldown(1, 10, commands.BucketType.user)

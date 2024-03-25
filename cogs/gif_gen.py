e24517c050a314616e532814418b2684d371e733
VINCENT <55220731+vpaone59@users.noreply.github.com>
2024-03-25 13:23:35 -0400

    Adjusted how gif_gen functions with some more detailed logs:

--- cogs/gif_gen.py
+++ cogs/gif_gen.py
@@ -5,7 +5,7 @@ Gif Generator Cog for ObamaBot by Vincent Paone https://github.com/vpaone59
 import random
 import os
 import json
-import logging
+from logging_config import setup_logging
 from urllib import parse, request
 from discord.ext import commands
 
@@ -13,26 +13,16 @@ from discord.ext import commands
 giphy_key = os.getenv("GIPHY_KEY")
 URL = "http://api.giphy.com/v1/gifs/search"
 
-logger = logging.getLogger(__name__)
+logger = setup_logging(__name__)
 
 
-class GifGenerator(commands.Cog):
+class Gif_Generator(commands.Cog):
     """
     giphy api - gif generator via query
     """
 
     def __init__(self, bot):
         self.bot = bot
-
-        # Configure the logger to save logs to bot.log
-        if not logger.handlers:
-            file_handler = logging.FileHandler("bot.log")
-            file_handler.setFormatter(
-                logging.Formatter(
-                    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-                )
-            )
-            logger.addHandler(file_handler)
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -49,23 +39,32 @@ class GifGenerator(commands.Cog):
         Picks 10 gifs and then randomly chooses 1 to send
         """
         params = parse.urlencode({"q": query, "api_key": giphy_key, "limit": "10"})
+
         try:
             with request.urlopen("".join((URL, "?", params))) as response:
                 data = json.loads(response.read())
-                # if there are no gifs returned we edit the bot's response
-                if len(data["data"]) == 0:
+
+                # Check if no GIFs were found
+                if not data.get("data"):
                     raise Exception(
-                        f"No gifs found for {query}, try again with a different search query"
+                        f"No GIFs found for {' '.join(query)}, try again with a different search query"
                     )
+
+                # Choose a random GIF from the list returned
+                selection = random.choice(data["data"])
+                link = selection.get("embed_url")
+                if link:
+                    await ctx.send(link)
                 else:
-                    selection = random.randint(0, len(data["data"]) - 1)
-                    link = data["data"][selection]["embed_url"]
-                    await ctx.send(f"{link}")
+                    raise Exception("No embed URL found for the selected GIF")
+
         except Exception as e:
-            logger.error(f"USER: {ctx.message.author} ERROR: {e}")
+            logger.error(
+                f"\n\t USER: {ctx.message.author}\n\t INPUT: {query}\n\t ERROR: {e}"
+            )
             await ctx.send(f"```{e}```")
 
 
 async def setup(bot):
     """ """
-    await bot.add_cog(GifGenerator(bot))
+    await bot.add_cog(Gif_Generator(bot))

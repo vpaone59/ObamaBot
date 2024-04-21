5f208d6b1b5627cfa2ef76b2158fe4bc2b82973d
vinny <vpaone59@gmail.com>
2024-04-20 20:23:07 -0400

    formatted the start and end time/date

--- cogs/predecessor_stats.py
+++ cogs/predecessor_stats.py
@@ -2,10 +2,10 @@
 https://github.com/vpaone59
 """
 
-import requests
-from discord.ext import commands
 import logging
-
+import requests
+from datetime import datetime
+from discord.ext import commands
 
 logger = logging.getLogger(__name__)
 
@@ -31,13 +31,14 @@ class PredecessorStats(commands.Cog):
         player_name = " ".join(player_name)
         player_info = await get_player_general(await get_player_id(player_name))
 
-        response_message = f"Player ID: {player_info['id']}\n\
-            Display Name: {player_info['display_name']}\n\
-            Region: {player_info['region']}\n\
-            Rank: {player_info['rank']}\n\
-            Active Rank: {player_info['rank_active']}\n\
-            Rank Title: {player_info['rank_title']}\n\
-            MMR: {player_info['mmr']}"
+        response_message = f"""
+Player ID: {player_info['id']}
+Display Name: {player_info['display_name']}
+Region: {player_info['region']}
+Rank: {player_info['rank']}
+Active Rank: {player_info['rank_active']}
+Rank Title: {player_info['rank_title']}
+MMR: {player_info['mmr']}"""
 
         await ctx.channel.send(response_message)
 
@@ -48,10 +49,9 @@ class PredecessorStats(commands.Cog):
         """
         player_name = " ".join(player_name)
         player_id = await get_player_id(player_name)
-        print(player_id, player_name)
+
         if player_id:
             player_match_history = await get_player_match_history(player_id)
-            print(player_match_history)
             response_message = f"""
 Start Time: {player_match_history['start_time']}
 End Time: {player_match_history['end_time']}
@@ -65,8 +65,7 @@ Deaths: {player_match_history['deaths']}
 Assists: {player_match_history['assists']}
 Total Damage Dealt to Heroes: {player_match_history['total_damage_dealt_to_heroes']}
 Total Damage Taken from Heroes: {player_match_history['total_damage_taken_from_heroes']}
-Gold Earned: {player_match_history['gold_earned']}
-                                """
+Gold Earned: {player_match_history['gold_earned']}"""
             await ctx.channel.send(response_message)
         else:
             await ctx.channel.send(f"Player '{player_name}' not found.")
@@ -121,8 +120,8 @@ async def get_player_match_history(player_id):
             for player in match["players"]:
                 if player["id"] == player_id:
                     player_info = player
-                    player_info["start_time"] = match["start_time"]
-                    player_info["end_time"] = match["end_time"]
+                    player_info["start_time"] = format_date(match["start_time"])
+                    player_info["end_time"] = format_date(match["end_time"])
                     break
             if player_info:
                 break
@@ -130,6 +129,14 @@ async def get_player_match_history(player_id):
     except requests.RequestException as e:
         logger.error("Error fetching data: %s", e)
         return None
+
+
+def format_date(date_string):
+    """
+    Formats the date string into a readable format.
+    """
+    date_object = datetime.fromisoformat(date_string[:-1])
+    return date_object.strftime("%B %d, %Y %I:%M:%S %p")
 
 
 async def setup(bot):

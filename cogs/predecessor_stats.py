48644d60bd557abb7dc9d3b7a886e9189d6675ed
vinny <vpaone59@gmail.com>
2024-04-24 16:25:33 -0400

    updated predecessor

--- cogs/predecessor_stats.py
+++ cogs/predecessor_stats.py
@@ -23,27 +23,26 @@ class PredecessorStats(commands.Cog):
         """ """
         logger.info("%s ready", self)
 
-    @commands.command(aliases=["pred"])
-    async def pred_player_stats(self, ctx, *player_name):
+    @commands.command(aliases=["stats"])
+    async def get_player_stats(self, ctx, *player_name):
         """
         Sends general information about the Predecessor player to the channel
         """
         player_name = " ".join(player_name)
-        player_info = await get_player_general(await get_player_id(player_name))
+        player_info = await get_player(await get_player_id(player_name))
 
         response_message = f"""
 Player ID: {player_info['id']}
 Display Name: {player_info['display_name']}
 Region: {player_info['region']}
-Rank: {player_info['rank']}
-Active Rank: {player_info['rank_active']}
-Rank Title: {player_info['rank_title']}
+Leaderboard: #{player_info['rank_active']}
+Rank: {player_info['rank_title']}
 MMR: {player_info['mmr']}"""
 
         await ctx.channel.send(response_message)
 
-    @commands.command(aliases=["predmh"])
-    async def pred_player_matches(self, ctx, *player_name):
+    @commands.command(aliases=["lg"])
+    async def get_last_match_stats(self, ctx, *player_name):
         """
         Sends the match history of a Predecessor player to the channel
         """
@@ -71,13 +70,13 @@ Gold Earned: {player_match_history['gold_earned']}"""
             await ctx.channel.send(f"Player '{player_name}' not found.")
 
 
-async def get_player_general(player_id):
+async def get_player(player_id):
     """
     Fetches general information about the player from omeda.city API.
     """
     url = f"https://omeda.city/players/{player_id}.json"
     try:
-        response = requests.get(url)
+        response = requests.get(url, timeout=15)
         response.raise_for_status()  # Raise an error for bad status codes
         return response.json()
     except requests.RequestException as e:
@@ -92,7 +91,7 @@ async def get_player_id(player_name):
     url = "https://omeda.city/players.json"
     params = {"filter[name]": player_name}
     try:
-        response = requests.get(url, params=params)
+        response = requests.get(url, params=params, timeout=15)
         response.raise_for_status()  # Raise an error for bad status codes
         players_data = response.json()
         if players_data:
@@ -112,7 +111,7 @@ async def get_player_match_history(player_id):
     url = f"https://omeda.city/players/{player_id}/matches.json"
     params = {"time_frame": "1D"}
     try:
-        response = requests.get(url, params=params)
+        response = requests.get(url, params=params, timeout=15)
         response.raise_for_status()  # Raise an error for bad status codes
         matches_data = response.json()["matches"]
         player_info = None
@@ -127,7 +126,7 @@ async def get_player_match_history(player_id):
                 break
         return player_info
     except requests.RequestException as e:
-        logger.error("Error fetching data: %s", e)
+        logger.error("Error fetching data: %s : Player Id - %s", e, player_id)
         return None
 
 

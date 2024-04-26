2d2d39525e5848fbce45d75a2c42a138f9cabadf
vinny <vpaone59@gmail.com>
2024-04-25 21:50:37 -0400

    lil format update

--- cogs/predecessor_stats.py
+++ cogs/predecessor_stats.py
@@ -31,13 +31,13 @@ class PredecessorStats(commands.Cog):
         player_name = " ".join(player_name)
         player_info = await get_player(await get_player_id(player_name))
 
-        response_message = f"""
+        response_message = f"""```
 Player ID: {player_info['id']}
 Display Name: {player_info['display_name']}
 Region: {player_info['region']}
 Leaderboard: #{player_info['rank_active']}
 Rank: {player_info['rank_title']}
-MMR: {player_info['mmr']}"""
+MMR: {player_info['mmr']}```"""
 
         await ctx.channel.send(response_message)
 
@@ -68,8 +68,7 @@ Deaths: {player_match_history['deaths']}
 Assists: {player_match_history['assists']}
 Total Damage Dealt to Heroes: {player_match_history['total_damage_dealt_to_heroes']}
 Total Damage Taken from Heroes: {player_match_history['total_damage_taken_from_heroes']}
-Gold Earned: {player_match_history['gold_earned']}
-```"""
+Gold Earned: {player_match_history['gold_earned']}```"""
             await ctx.channel.send(response_message)
         else:
             await ctx.channel.send(f"Player '{player_name}' not found.")

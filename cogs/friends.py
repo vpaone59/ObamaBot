a1bc4e73df94943b2c0bdc7fc2807ffe68c5c4e9
vinny <vpaone59@gmail.com>
2024-04-30 22:18:40 -0400

    removed long term unused commands that were too specific

--- cogs/friends.py
+++ cogs/friends.py
@@ -9,14 +9,10 @@ import random
 import logging
 import discord
 from discord.ext import commands
+from logging_config import setup_logging
 
-logger = logging.getLogger(__name__)
+logger = setup_logging(__name__)
 
-BEE_REACTS = [
-    "<:obamagiga:844300314936213565> :point_right: :bee:",
-    ":bee: :broom: <:feelsobama:842634906999193620>",
-    "<:obamajoy:842822700912476190> :fire: :bee: :fire:",
-]
 FRIEND_GUILDS = os.getenv("FRIEND_GUILD_IDS")
 
 
@@ -30,7 +26,6 @@ class Friends(commands.Cog):
         self.bot = bot
         self.ethan_enabled = False  # Flag for Ethan reactions
         self.walter_enabled = False  # Flag for Walter reactions
-        self.dj_enabled = False  # Flag for DJ functionality
         self.inc_enabled = False  # Flag for INC functionality
 
     @commands.Cog.listener()
@@ -66,15 +61,6 @@ class Friends(commands.Cog):
                     file=discord.File("gifs/weird/boner_alert.jpg")
                 )
 
-        # # if DJ (ID= 123107464240562180) is @ mentioned
-        # for user in message.mentions:
-        #     if user.id == 123107464240562180:
-        #         file_name = get_random_batman_deej_file()
-        #         if file_name:
-        #             file_path = os.path.join("gifs/memes", file_name)
-        #             await message.channel.send(file=discord.File(file_path))
-        #         break  # Once the user is found, we can break out of the loop
-
         # if message.guild.id == 842545435050508328:
         #     if string_message.startswith("INC"):
         #         format_link = format_incident_link(string_message)
@@ -104,76 +90,12 @@ class Friends(commands.Cog):
         else:
             await ctx.send("Walter reactions disabled.")
 
-    @commands.command()
-    @commands.cooldown(1, 10, commands.BucketType.user)
-    async def toggle_dj(self, ctx):
-        """
-        Toggle the DJ functionality on or off.
-        """
-        self.dj_enabled = not self.dj_enabled
-        if self.dj_enabled:
-            await ctx.send("DJ functionality enabled. Mention me to play memes!")
-        else:
-            await ctx.send("DJ functionality disabled.")
-
-    @commands.command(aliases=["bee", "b"])
-    @commands.cooldown(1, 10, commands.BucketType.user)
-    async def bees(self, ctx):
-        """
-        Random reaction for a bees command
-        """
-        try:
-            await ctx.channel.send(random.choice(BEE_REACTS))
-        except Exception as e:
-            logger.error(f"USER: {ctx.message.author} ERROR: {e}")
-            await ctx.channel.send(f"Error {__name__}: {e}")
-
-    @commands.command(aliases=["reeve", "reevez", "reeves!"])
-    @commands.cooldown(1, 10, commands.BucketType.user)
-    async def reeves(self, ctx):
-        """
-        Send a picture of Reeves!
-        """
-        try:
-            await ctx.channel.send(file=discord.File("gifs/lads/reeves_gun_permit.png"))
-        except Exception as e:
-            logger.error(f"USER: {ctx.message.author} ERROR: {e}")
-            await ctx.channel.send(f"Error {__name__}: {e}")
-
-    @commands.command(aliases=["dumpydave"])
-    @commands.cooldown(1, 10, commands.BucketType.user)
-    async def dumpy(self, ctx):
-        """
-        Send a picture of Dave's fat ass
-        """
-        try:
-            await ctx.channel.send(file=discord.File("gifs/lads/dumpy_dave_2.png"))
-        except Exception as e:
-            logger.error(f"USER: {ctx.message.author} ERROR: {e}")
-            await ctx.channel.send(f"Error {__name__}: {e}")
-
 
 def format_incident_link(incident_number):
     """
     Format incident number to a clickable link
     """
     return f"https://s.rowan.edu/{incident_number}"
-
-
-def get_random_batman_deej_file():
-    """
-    Get all files in the memes directory that are of .png and contain 'batman_deej' in the filename
-    """
-    folder_path = "gifs/memes"
-    files = [
-        file
-        for file in os.listdir(folder_path)
-        if file.endswith(".png") and "batman_deej" in file
-    ]
-    if files:
-        return random.choice(files)
-    else:
-        return None
 
 
 async def setup(bot):

7af8684a6d9aff5542e339d2080d0db2a70e10dc
vinny <vpaone59@gmail.com>
2024-05-09 23:24:39 -0400

    added dockerignore and updated some comments and such in the entrypoint and sample.env

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 
 # Run the SQL script using SQLite
 sqlite3 ./database/obamabot.db < ./database/init_create.sql
-echo "database created at -> "
+echo "database created at -> ./database/"
 
 # Run the bot
 python3 main.py
+echo "Bot started -> python3 main.py"

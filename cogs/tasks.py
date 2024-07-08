9b723f83f2e6144b264669356e4312e0cc3a9388
vinny <vpaone59@gmail.com>
2024-07-07 22:35:43 -0400

    ok but now i actually added the ability to call the task with any channel id

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -4,6 +4,7 @@ Tasks Cog for ObamaBot by Vincent Paone https://github.com/vpaone59
 Setup tasks to run periodically in your server.
 """
 
+import os
 from datetime import datetime
 from discord.ext import commands, tasks
 from cogs.youtube import (
@@ -13,6 +14,7 @@ from cogs.youtube import (
 from logging_config import create_new_logger
 
 logger = create_new_logger(__name__)
+RMT_CHANNEL_ID = os.getenv("RMT_CHANNEL_ID")
 
 
 class Tasks(commands.Cog):
@@ -22,8 +24,7 @@ class Tasks(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.tasks = {}
-        self.task_channels = {}
+        self.running_tasks = {}
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -31,82 +32,87 @@ class Tasks(commands.Cog):
         Runs when the cog is loaded
         """
         logger.info(f"{self} ready")
-        try:
-            self.ratemytakeaway_task.start()
-            logger.info("ratemytakeaway_task started")
-        except Exception as e:
-            logger.error("Error starting ratemytakeaway_task : %s", e)
 
     @commands.command(aliases=["start task", "run task", "start", "run"])
     async def start_task(self, ctx, task_name: str, channel_id: int = None):
         """
-        Start a task with the given name.
+        Start a task in a specific channel or the channel the command was run in by default.
         """
-        # Check if the task exists and is a Loop task
+        # Check if the task exists and is an instance of tasks.Loop
         task = getattr(self, task_name, None)
         if task is None or not isinstance(task, tasks.Loop):
             await ctx.send(f"No task found with the name {task_name}.")
+            logger.info("No task found with the name %s", task_name)
             return
 
         # Check if the task is already running
         if task.is_running():
             await ctx.send(f"The task {task_name} is already running.")
+            logger.info("Task %s is already running", task_name)
             return
 
-        if channel_id is not None:
-            channel = self.bot.get_channel(channel_id)
-            if channel is None:
-                await ctx.send(f"Could not find channel with ID {channel_id}.")
-                return
+        # Get the channel to send messages to from the channel ID. If no channel ID is provided, use the channel the command was run in.
+        channel = self.bot.get_channel(channel_id) if channel_id else ctx.channel
 
-        # Start the task
-        task.start()
+        # If channel is None, the channel ID provided is invalid or the bot does not have access to the channel. Send an error message and return.
+        if channel is None:
+            await ctx.send(f"Could not find channel with ID {channel_id}.")
+            logger.info("Could not find channel with ID %s", channel_id)
+            return
 
-        # Store the channel ID with the task
-        self.tasks[task_name] = task
-        self.task_channels[task_name] = (
-            channel if channel_id is not None else ctx.channel.id
-        )
+        # Store the task name and channel ID in the running_tasks dictionary
+        self.running_tasks[task_name] = {"channel": channel}
 
-        logger.info(
-            "Task %s started by %s in channel %s",
-            task_name,
-            ctx.author.id,
-            ctx.channel.id,
-        )
+        # Send a message to the channel where the start task command was ran from
         await ctx.send(f"The task {task_name} has been started.")
+        logger.info(
+            "Task %s started by user %s in channel %s",
+            task_name,
+            ctx.author.name,
+            channel,
+        )
+
+        # Start the task. This is the last thing done because it will also run the task, so we wait until the end to start it.
+        try:
+            task.start()
+        except Exception as e:
+            await ctx.send(f"ERROR {e}")
+            logger.error("Error starting task %s: %s", task_name, e)
 
     @commands.command(aliases=["stop task", "end task", "stop", "end"])
     async def stop_task(self, ctx, task_name: str):
         """
         Stop a task with the given name.
         """
-        task = self.tasks.get(task_name)
+        task = self.running_tasks.get(task_name)
         if task is None:
             await ctx.send(f"Task '{task_name}' is not running in this channel")
             return
 
         try:
             task.cancel()
-            del self.tasks[task_name]
-            logger.info("Task %s stopped by %s", task_name, ctx.author.id)
+            del self.running_tasks[task_name]
             await ctx.send(f"Task '{task_name}' stopped")
+            logger.info("Task %s stopped by %s", task_name, ctx.author.id)
         except Exception as e:
-            logger.error("Error stopping task %s: %s", task_name, e)
             await ctx.send(f"ERROR {e}")
+            logger.error("Error stopping task %s: %s", task_name, e)
 
     @commands.command(aliases=["tasks", "status"])
     async def show_tasks_status(self, ctx):
         """
         Show the status and details of all tasks.
         """
-        if not self.tasks:
+        print("ok")
+        # Check if there are any tasks running
+        if not self.running_tasks:
             await ctx.send("No tasks have been started yet.")
             return
 
-        for task_name, task in self.tasks.items():
+        # Iterate through all the tasks and send a message with the status of each task
+        for task_name, task in self.running_tasks.items():
             status = "running" if task.is_running() else "stopped"
-            channel_id = self.task_channels.get(task_name)
+            channel_id = self.running_tasks.get(task_name)
             channel_info = f" (in channel <#{channel_id}>)" if channel_id else ""
 
             next_iteration = task.next_iteration
@@ -127,19 +133,13 @@ class Tasks(commands.Cog):
         """
         # Check if the current time is 6:05 PM to fetch the latest video
         now = datetime.now()
-        if now.hour == 14 and now.minute == 5:
-            # Get the channel ID associated with the task
-            channel_id = self.task_channels.get("ratemytakeaway_task")  # Get channel ID
-            if channel_id is None:
+        if now.hour == 22:
+            rmt_channel_info = self.running_tasks.get("ratemytakeaway_task")
+            rmt_channel = rmt_channel_info["channel"]
+            if rmt_channel is None:
                 logger.error(
-                    "No channel associated with ratemytakeaway_task, but task is running. I would stop the task to avoid errors."
+                    "No channel associated with ratemytakeaway_task, but task is running."
                 )
-                return
-
-            channel = self.bot.get_channel(channel_id)
-
-            if channel is None:
-                logger.error("Could not find channel with ID - %s", channel_id)
                 return
 
             # Check if the current time is 6:05 PM to fetch the latest video
@@ -151,7 +151,9 @@ class Tasks(commands.Cog):
                 "Fetched the latest video from Rate My Takeaway's channel - %s",
                 latest_video,
             )
-            await channel.send(f"{latest_video['title']} - {latest_video['video_url']}")
+            await rmt_channel.send(
+                f"{latest_video['title']} - {latest_video['video_url']}"
+            )
 
 
 async def setup(bot):

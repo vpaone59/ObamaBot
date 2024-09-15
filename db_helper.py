b4e61369bb209a1507732e91d83507a074bd51ad
vinny <vpaone59@gmail.com>
2024-09-14 20:50:00 -0400

    fixed db helper

--- db_helper.py
+++ db_helper.py
@@ -7,9 +7,9 @@ load_dotenv()
 
 logger = create_new_logger(__name__)
 
-DATABASE_PATH = os.getenv("DATABASE_PATH")
-SQL_INIT_FILE = DATABASE_PATH + "/init_create.sql"
-print("SQL_INIT_FILE", SQL_INIT_FILE)
+HOST_DB_PATH = os.getenv("HOST_DB_PATH")
+DATABASE_PATH = f"{HOST_DB_PATH}/obamabot.db"
+SQL_INIT_FILE = "./database/init_create.sql"
 
 
 def get_db_connection():
@@ -17,14 +17,11 @@ def get_db_connection():
     Connect to the SQLite database using the path from environment variables.
     Returns a connection object.
     """
-    if not DATABASE_PATH:
-        logger.error("DATABASE_PATH environment variable not set.")
-        raise ValueError("DATABASE_PATH environment variable not set.")
-
     try:
         conn = sqlite3.connect(DATABASE_PATH)
-        logger.info("Connected to database")
+        logger.info("Connected to database at %s", DATABASE_PATH)
         return conn
+
     except sqlite3.Error as e:
         logger.error("Failed to connect to database: %s", e)
         raise e
@@ -34,8 +31,23 @@ def initialize_db():
     """
     Initialize the database with the required tables using the init_create.sql file.
     """
-    conn = get_db_connection()
-    cursor = conn.cursor()
+    # Check if the database file exists
+    try:
+        if not os.path.exists(DATABASE_PATH):
+            logger.info("Database file does not exist. Creating now...")
+
+            # Create the database file
+            with sqlite3.connect(DATABASE_PATH) as conn:
+                logger.info("Database file created successfully")
+
+            conn.close()
+
+        else:
+            logger.info("Database file already exists")
+
+    except Exception as e:
+        logger.error("Failed to create database file: %s", e)
+        raise e
 
     # Read the SQL initialization file
     with open(SQL_INIT_FILE, "r") as sql_file:
@@ -43,15 +55,23 @@ def initialize_db():
 
     # Execute the SQL script
     try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
         cursor.executescript(sql_script)
         conn.commit()
         logger.info("Database initialized successfully")
+
+    except sqlite3.OperationalError as e:
+        if "already exists" in str(e):
+            logger.warning("Tables already exist. Skipping setup.")
+        else:
+            logger.error("Failed to initialized database: %s", e)
+            raise e
+
     except sqlite3.Error as e:
-        logger.error("Failed to initialize database: %s", e)
+        logger.error("Failed to initialized database: %s", e)
         raise e
+
     finally:
-        conn.close()
-
-
-if __name__ == "__main__":
-    initialize_db()
+        if conn:
+            conn.close()

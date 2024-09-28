3e3190e60858890adee357576fab20048aa93e00
vinny <vpaone59@gmail.com>
2024-09-28 19:13:20 -0400

    updated sql init script. starting to use slash commands more

--- cogs/list_maker.py
+++ cogs/list_maker.py
@@ -2,6 +2,8 @@
 https://github.com/vpaone59
 """
 
+import discord
+from discord import app_commands
 from discord.ext import commands
 from logging_config import create_new_logger
 from db_helper import get_db_connection
@@ -24,17 +26,27 @@ class ListMaker(commands.Cog):
         """
         logger.info("%s ready", self)
 
-    @commands.command(name="create_list")
-    async def create_list(self, ctx, *list_name: str):
+    @app_commands.command(
+        name="create_list",
+        description="Create a custom list that only you can add items to",
+    )
+    async def create_user_list(self, interaction: discord.Interaction, list_name: str):
         """
         Create a new list
         """
-        user_id = ctx.author.id
-        print("user_id", user_id)
-        print("list_name", list_name)
+        # Grab the user's ID from the interaction
+        user_id = interaction.user.id
 
+        # Get a connection to the database
         conn = get_db_connection()
         cursor = conn.cursor()
+
+        # Check if the list already exists
+        if self.does_list_exist(user_id, list_name):
+            await interaction.response.send_message(
+                f'List "{list_name}" already exists.'
+            )
+            return
 
         try:
             cursor.execute(
@@ -42,39 +54,82 @@ class ListMaker(commands.Cog):
                 (user_id, list_name),
             )
             conn.commit()
-            await ctx.send(f'List "{list_name}" created.')
+            await interaction.response.send_message(f'List "{list_name}" created.')
         except Exception as e:
-            await ctx.send(f'Failed to create list "{list_name}": {e}')
+            await interaction.response.send_message(
+                f'Failed to create list "{list_name}": {e}'
+            )
         finally:
             conn.close()
 
-    @commands.command(name="add_item")
-    async def add_item_to_list(self, ctx, list_name: str, *, item: str):
+    @app_commands.command(name="add_item_to_list", description="Add an item to a list")
+    async def add_item_to_list(
+        self, interaction: discord.Interaction, list_name: str, item: str
+    ):
         """
         Add an item to a list
         """
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute(
+                "INSERT INTO list_items (user_id, list_name, item_name) VALUES (?, ?, ?)",
+                (interaction.author.id, list_name, item),
+            )
+            conn.commit()
+            await interaction.send(f'Item "{item}" added to list "{list_name}"')
+        except Exception as e:
+            await interaction.send(
+                f'Failed to add item "{item}" to list "{list_name}": {e}'
+            )
+
+    @app_commands.command(
+        name="remove_item_from_list", description="Remove an item from a list"
+    )
+    async def remove_item_from_list(
+        self, interaction: discord.Interaction, list_name: str, item: str
+    ):
         pass
 
-    @commands.command(name="remove_item")
-    async def remove_item_from_list(self, ctx, list_name: str, *, item: str):
+    def get_user_list_items(self, user_id, list_name: str):
         """
-        Remove an item from a list
+        Get all list items for a user's list
         """
-        pass
+        conn = get_db_connection()
+        cursor = conn.cursor()
 
-    @commands.command(name="show_list")
-    async def show_list(self, ctx, list_name: str):
-        """
-        Display the list
-        """
-        pass
+        try:
+            cursor.execute(
+                "SELECT item_name FROM list_items WHERE user_id = ? AND list_name = ?",
+                (user_id, list_name),
+            )
+            result = cursor.fetchall()
+            print(result)
+            return result
+        except Exception as e:
+            logger.error("Failed to get list items: %s", e)
+            return
 
-    @commands.command(name="show_all_lists")
-    async def show_all_lists(self, ctx):
+    def does_list_exist(self, user_id, list_name: str):
         """
-        Display all lists for the user
+        Check if a list already exists in the database
         """
-        pass
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        try:
+            # Check if the list name already exists for the user
+            cursor.execute(
+                "SELECT 1 FROM lists WHERE user_id = ? AND list_name = ?",
+                (user_id, list_name),
+            )
+            result = cursor.fetchone()
+            return bool(result)
+
+        except Exception as e:
+            logger.error("Failed to check if list exists: %s", e)
+            return False
 
 
 async def setup(bot):
--- database/init_create.sql
+++ database/init_create.sql
@@ -11,13 +11,12 @@ CREATE TABLE users (
 
 CREATE TABLE lists (
   list_id INTEGER PRIMARY KEY,
-  list_name VARCHAR(255) NOT NULL,
-  list_items TEXT NOT NULL
+  user_id VARCHAR(255) NOT NULL,
+  list_name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE user_lists (
-  list_id INTEGER PRIMARY KEY,
-  user_id INTEGER NOT NULL,
-  list_name VARCHAR(255) NOT NULL,
-  list_items TEXT NOT NULL
+CREATE TABLE items (
+  item_id INTEGER PRIMARY KEY,
+  list_id INTEGER NOT NULL,
+  item_name VARCHAR(255) NOT NULL
 );

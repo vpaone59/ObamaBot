daa3042e5fdebfc353ae1bf86eb189b0aa20fc04
vinny <vpaone59@gmail.com>
2025-05-14 19:06:41 -0400

    Removed some print statements

--- cogs/general.py
+++ cogs/general.py
@@ -119,12 +119,10 @@ def fibonacci(n):
     calculate fibonacci sequence for a given n
     """
     if n < 0:
-        print(f"User input {n}: negative not allowed ")
+        return "Invalid input"
     elif n == 0:
-        print(f"User input {n}: Fib({n}) = 0")
         return 0
     elif n == 1 or n == 2:
-        print(f"User input {n}: Fib({n}) = 1")
         return 1
     else:
         fib = fibonacci(n - 1) + fibonacci(n - 2)
